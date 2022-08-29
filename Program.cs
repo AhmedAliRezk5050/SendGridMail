c5967011d7b4c8fb8caad82baac0b54c805a7f80
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-29 15:27:08 +0200

    Make the project more configurable

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
-﻿using SendGrid;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SendGrid;
 using SendGrid.Helpers.Mail;
 
-var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
-var client = new SendGridClient(apiKey);
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+var config = host.Services.GetRequiredService<IConfiguration>();
+
+var apiKey = config.GetValue<string>("SendGridApiKey");
+var fromEmail = config.GetValue<string>("FromEmail");
+var fromName = config.GetValue<string>("FromName");
+if (string.IsNullOrEmpty(apiKey)) throw new Exception("SendGridApiKey should not be null or empty");
+if (string.IsNullOrEmpty(fromEmail)) throw new Exception("FromEmail should not be null or empty");
+if (string.IsNullOrEmpty(fromName)) throw new Exception("FromName should not be null or empty");
 
 Console.Write("To: ");
 var toEmail = Console.ReadLine();
@@ -13,10 +23,10 @@ var subject = Console.ReadLine();
 Console.Write("Body: ");
 var body = Console.ReadLine();
 
-
+var client = new SendGridClient(apiKey);
 var msg = new SendGridMessage()
 {
-    From = new EmailAddress("ahmedalirezk5050@gmail.com", "ahmed ali"),
+    From = new EmailAddress(fromEmail, fromName),
     Subject = subject,
     PlainTextContent = body
 };
@@ -25,7 +35,4 @@ var msg = new SendGridMessage()
 msg.AddTo(new EmailAddress(toEmail));
 var response = await client.SendEmailAsync(msg);
 
-// A success status code means SendGrid received the email request and will process it.
-// Errors can still occur when SendGrid tries to send the email. 
-// If email is not received, use this URL to debug: https://app.sendgrid.com/email_activity 
 Console.WriteLine(response.IsSuccessStatusCode ? "Email queued successfully!" : "Something went wrong!");
